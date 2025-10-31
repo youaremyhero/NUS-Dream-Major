7172cd7d9a5bf6ad4b1332af519ca16faaf40694
youaremyhero <87740611+youaremyhero@users.noreply.github.com>
2025-10-31 10:10:54 +0800

    Update nextSteps.js

--- js/content/nextSteps.js
+++ js/content/nextSteps.js
@@ -1,1 +1,26 @@
-
+export const NEXT_STEPS = [
+  {
+    id: "chat-students",
+    title: "Chat with Current Students",
+    description:
+      "Chat with our current students to learn about their invigorating and vibrant campus life that is uniquely NUS.",
+    ctaLabel: "Scedule a chat today",
+    url: "https://www.nus.edu.sg/oam/nus-experience/chat-with-current-nus-students",
+  },
+  {
+    id: "campus-experience",
+    title: "Experience campus life",
+    description:
+      "Join open houses, student club events and webinars to see how learning, living and leadership connect on campus.",
+    ctaLabel: "Discover enriching experiences",
+    url: "https://www.nus.edu.sg/oam/nus-experience",
+  },
+  {
+    id: "research-application",
+    title: "Plan your application",
+    description:
+      "Use our admissions resources to learn more about the deadlines, admission requirements, and other important information.",
+    ctaLabel: "Find out more",
+    url: "https://www.nus.edu.sg/oam/admissions",
+  }
+];
